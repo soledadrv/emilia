9360c726945318e66a5d42d691ff1466daf4814a
soledadrv <soledadrv@outlook.com>
2021-07-29 19:56:35 -0300

    maquetado de about

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const path = require('path');
 app.use(express.static('public'));
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/about',(req,res) => res.sendFile(path.join(__dirname,'views','about.html')));
+app.get('/music',(req,res) => res.sendFile(path.join(__dirname,'views','music.html')));
+app.get('/contact',(req,res) => res.sendFile(path.join(__dirname,'views','contact.html')));
 
 
 app.listen(port,() => console.log('Servidor funcionando en el puerto ' + port));
